b6f1a031218363033db0f8ea3836209decdef9ff
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-03-25 16:59:23 +0300

    added types

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,6 +1,8 @@
-export const resolvers = {
+import { Resolvers } from "./types"
+
+export const resolvers: Resolvers = {
     Query:{
-        featuredPlaylists:( _, _, {datasource}:any ) =>{
+        featuredPlaylists:( _, _, {datasource}) =>{
             return datasource.SpotifyAPI.getFeaturedPlaylists()
 
         }
