445faf158464a8a8fa753f65503a9a3af0b1a679
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-04-03 14:53:02 +0300

    added playlist track query

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,12 +1,30 @@
-import { Resolvers } from "./types"
+import { Resolvers, Tracks } from "./types"
 
 export const resolvers: Resolvers = {
     Query: {
         featuredPlaylists: (_, __, { dataSource }) => {
             return dataSource.spotifyAPI.getFeaturedPlaylists();
         },
-        playlist : (_, {id}, {dataSource}) => {
-            return dataSource.spotifyAPI.getPlaylist(id)
-        },
+        playlist: async (_, { id }, { dataSource }) => {
+            // const {
+            //   id: playlistId,
+            //   name,
+            //   description,
+            //   tracks: { items = [] } = {},
+            // } = await dataSource.spotifyAPI.getPlaylist(id);
+          
+            // const newTrackItems = items.map(({ track }: { track: Tracks }) => {
+            //   const { id, name, durationMs, explicit, url } = track;
+            //   return { id, name, durationMs: durationMs, explicit, url };
+            // });
+          
+            return dataSource.spotifyAPI.getPlaylist(id);
+          },
+    },
+    Playlist: {
+        tracks: ({tracks}) => {
+            const {items = [] } = tracks
+            return items.map(({track}) => track);
+        }
     }
 }
