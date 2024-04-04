2ecd785b137bd886c2668ca7231920e4fe3871af
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-04-04 11:29:24 +0300

    imported snapshot-or-error model and added it to addItemsToPlaylist function

--- src/datasource/spotify-api.ts
+++ src/datasource/spotify-api.ts
@@ -1,6 +1,6 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
 import { Playlist } from "../types";
-import { PlaylistModel } from "../models";
+import { PlaylistModel, SnapshotOrError } from "../models";
 
 export class SpotifyAPI extends RESTDataSource {
     baseURL= "https://spotify-demo-api-fe224840a08c.herokuapp.com/v1/";
@@ -14,7 +14,7 @@ export class SpotifyAPI extends RESTDataSource {
     getPlaylist(playlistId: string): Promise<PlaylistModel>{
       return this.get(`playlists/${playlistId}`)
     }
-    addItemsToPlaylist(input: {playlistId: string, uris: string[]}){
+    addItemsToPlaylist(input: {playlistId: string, uris: string[]}): Promise<SnapshotOrError>{
       const {playlistId, uris} = input
       return this.post(`playlists/${playlistId}/tracks`, {
         params:{
