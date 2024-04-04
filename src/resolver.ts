653bc725e99c64a4076083ccd154a18896de4ad4
Queenzy-Ongeye <queenzyblack176@gmail.com>
2024-04-04 12:55:01 +0300

    added mutation for addItems toPlaylist

--- src/resolver.ts
+++ src/resolver.ts
@@ -12,22 +12,51 @@ export const resolvers: Resolvers = {
             //   description,
             //   tracks: { items = [] } = {},
             // } = await dataSource.spotifyAPI.getPlaylist(id);
-          
+
             // const newTrackItems = items.map(({ track }: { track: Tracks }) => {
             //   const { id, name, durationMs, explicit, url } = track;
             //   return { id, name, durationMs: durationMs, explicit, url };
             // });
-          
+
             return dataSource.spotifyAPI.getPlaylist(id);
-          },
+        },
     },
     Playlist: {
-        tracks: ({tracks}) => {
-            const {items = [] } = tracks
-            return items.map(({track}) => track);
+        tracks: ({ tracks }) => {
+            const { items = [] } = tracks
+            return items.map(({ track }) => track);
         }
     },
-    Tracks :{
+    Tracks: {
         duration_ms: (parent) => parent.duration_ms
+    },
+    Mutation: {
+        addItemsToPlaylist: async (_, { input }, { dataSource }) => {
+            try {
+                const response = await dataSource.spotifyAPI.addItemsToPlaylist(input);
+                if (response.snapshot_id) {
+                    return {
+                        code: 200,
+                        success: true,
+                        message: "Tracks added to playlist!",
+                        playlistId: response.snapshot_id,
+                    };
+                } else {
+                    throw Error("snapshot_id property not found");
+                }
+            } catch (e) {
+                return {
+                    code: 500,
+                    success: false,
+                    message: `Something went wrong: ${e}`,
+                    playlistId: null,
+                };
+            }
+        },
+    },
+    AddItemsToPlaylistPayload: {
+        playlist: ({playlistId}, _, {dataSource}) => {
+            return dataSource.spotifyAPI.getPlaylist(playlistId)
+        }
     }
 }
